73069c9e9c6a646dd7afa90344bce087fe6cdd40
Anis Ghliss <anisghliss20@gmail.com>
2017-11-20 18:42:12 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,10 @@
 def valid_move? (board,index)
 if position_taken?(board,index) == true
   true
-else 
+else
   nil
-
+end 
+end
 def position_taken? ( board , index )
 if (board[index] == " " || board[index] == "" || board[index] == nil)
  false
