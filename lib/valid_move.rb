3112a070f50f753665476839aaf9cc8e7a894e46
Anis Ghliss <anisghliss20@gmail.com>
2017-11-20 19:39:26 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,9 @@
 def valid_move? (board,index)
-if(index.between?(1,9) == false)
+if(index.between?(1,9) == false ||  position_taken?(board,index) )
 nil
 elsif (position_taken?(board,index) == false  || board[index-1] != " ")
   true
-elsif ( position_taken?(board,index)  )
-nil
+
 
 
 
