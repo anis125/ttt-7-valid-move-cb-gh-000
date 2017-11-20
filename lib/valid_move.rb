6201565218a15a03d974d66bc206dffd057c679e
Anis Ghliss <anisghliss20@gmail.com>
2017-11-20 19:23:39 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,8 @@
 def valid_move? (board,index)
 if(position_taken?(board,index) == false || board[index-1] != " " ||index.between(1,9))
   true
-else
+elsif( position_taken?(board,index )  
+  
   nil
 
 
